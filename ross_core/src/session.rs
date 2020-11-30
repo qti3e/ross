cdb0356ffc9c76d4296debf2250e39f06804bf6e
Parsa Ghadimi <qti3eqti3e@gmail.com>
2020-11-30 08:42:46 +0330

    Improve the type of session response

--- ross_core/src/session.rs
+++ ross_core/src/session.rs
@@ -41,8 +41,8 @@ pub struct Session {
     db: DBSync,
     snapshot: Snapshot,
     id: BranchIdentifier,
-    branch: BranchInfo,
     live_changes: Vec<BatchPatch>,
+    head: CommitHash,
 }
 
 impl Session {
@@ -50,7 +50,12 @@ impl Session {
     pub fn perform(&mut self, batch: BatchPatch) -> Result<Response> {
         let revert_patch = match self.snapshot.apply_batch_patch(&batch.patches, false) {
             Ok(revert_patch) => revert_patch,
-            Err(conflicts) => return Ok(Response::PerformConflicts(conflicts)),
+            Err(conflicts) => {
+                return Ok(Response {
+                    others: None,
+                    current: Some(Message::Conflicts(conflicts)),
+                })
+            }
         };
 
         let do_write = || -> Result<()> {
@@ -62,9 +67,10 @@ impl Session {
         match do_write() {
             Ok(()) => {
                 self.live_changes.push(batch);
-                Ok(Response::BroadcastBatchPatch(
-                    self.live_changes.last().unwrap(),
-                ))
+                Ok(Response {
+                    others: Some(Message::Patch(self.live_changes.last().unwrap())),
+                    current: None,
+                })
             }
             Err(error) => {
                 self.snapshot
@@ -76,17 +82,77 @@ impl Session {
     }
 
     /// Performs the initial sync.
-    pub fn sync(&self) -> Result<Response> {
-        Ok(Response::FullSync {
+    pub fn sync(&self) -> Message {
+        Message::FullSync {
             head: SessionHead {
-                commit: self.branch.head.unwrap().hash,
+                commit: self.head,
                 live: self.live_changes.len(),
             },
             snapshot: &self.snapshot,
-        })
+        }
     }
 
-    pub fn partial_sync(&mut self, head: SessionHead, batches: Vec<BatchPatch>) {}
+    /// A partial sync happens when reconnecting to the server after a period in which
+    /// the user was offline.
+    pub fn partial_sync(
+        &mut self,
+        head: SessionHead,
+        batches: Vec<BatchPatch>,
+    ) -> Result<Response> {
+        let same_commit = self.head == head.commit;
+        let same_live = self.live_changes.len() == head.live;
+
+        match (same_commit, same_live, batches.len()) {
+            (true, true, 0) => {
+                // There were no activities both on the server and the client while
+                // the user was offline. -> Don't do anything.
+                unimplemented!();
+            }
+            (true, true, _) => {
+                // There were no activities on the server while the user was offline,
+                // but there were changes made by the user in that period.
+                // -> Sync the server and other users.
+                unimplemented!();
+            }
+            (true, false, 0) => {
+                // There were no new commits, but there were some changes on the server,
+                // but on the side of the user.
+                // -> We only need to sync the current client.
+                unimplemented!();
+            }
+            (true, false, _) => {
+                // There were no new commits, but there were some changes on both server,
+                // and the client.
+                // -> Sync the current client & try to apply new changes made by the user
+                // and sync everyone else.
+                unimplemented!();
+            }
+            (false, _, 0) => {
+                // There was a commit, but no new changes were made by the user.
+                // So just send a full-sync response, the user will figure out the delta.
+                unimplemented!();
+            }
+            (false, _, _) => {
+                // There was a commit, and new changes were made by the user.
+                // So send a full-sync response, and apply patches by the user and
+                // update everyone else too.
+                unimplemented!();
+            }
+        }
+    }
+
+    /// Commit the live changes, it will only commit the changes if the head given
+    /// in parameters is still valid.
+    /// We may introduce `lock` and `unlock` to lock a session temporarily before
+    /// sending the commit request.
+    pub fn commit(
+        &mut self,
+        user: UserId,
+        head: SessionHead,
+        message: Message,
+    ) -> Result<Response> {
+        unimplemented!()
+    }
 }
 
 /// Head of the session.
@@ -98,18 +164,26 @@ pub struct SessionHead {
     live: usize,
 }
 
+/// A message sent from the server to the client.
 #[derive(Debug, Serialize)]
-pub enum Response<'a> {
-    /// Don't do anything.
-    None,
+pub enum Message<'a> {
     /// The perform request had conflicts and therefore was not applied.
-    /// Send this list of conflicts to the current user.
-    PerformConflicts(Vec<PatchConflict>),
-    /// Broadcast this patch to everyone except the current user.
-    BroadcastBatchPatch(&'a BatchPatch),
-    /// Needs to sent to the current user.
+    Conflicts(Vec<PatchConflict>),
+    /// A patch that needs to applied on the client side.
+    Patch(&'a BatchPatch),
+    /// An snapshot of the entire state of the session.
     FullSync {
         head: SessionHead,
         snapshot: &'a Snapshot,
     },
+}
+
+/// Result of an action on the session which is two optional messages,
+/// one of which needs to be broadcasted to everyone except the current
+/// user and the other needs to be sent to the user who initiated the
+/// request.
+#[derive(Debug, Serialize)]
+pub struct Response<'a> {
+    pub others: Option<Message<'a>>,
+    pub current: Option<Message<'a>>,
 }
